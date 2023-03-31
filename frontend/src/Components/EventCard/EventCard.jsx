4178e24c2aaf5b86cad77c2887b4a4d131deec1e
arthurgbsf <arthurgbsf@gmail.com>
2023-03-31 01:03:41 -0300

    Primeira configuração com o backend

--- frontend/src/Components/EventCard/EventCard.jsx
+++ frontend/src/Components/EventCard/EventCard.jsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 
 import EventFolder from "../../assets/event-folder.svg"
 
-function EventCard() {
+function EventCard({name, campus, startDate, endDate, details, folder, id}) {
   return (
       <div className="card shadow-border">
           <img src={EventFolder} className="card-img-top" alt="..."/>
             <div className="row align-items-center card-body">
               <h4 className=" col-12 card-title">
                 <strong>
-                Card title
+                  {name}
                 </strong>
               </h4>
               <div className='col-2 d-flex align-items-center justify-content-center'>
@@ -22,17 +22,19 @@ function EventCard() {
               </div>
               <div className='col-10 ps-0'>
                 <div className='adress-fontsize'>
-                  12/03/23
+                  {startDate}
                 </div>
               </div>
               <div className='col-2 d-flex align-items-center justify-content-center'>
                 <GoLocation className='icon-size'/>
               </div>
               <div className='col-10 ps-0'>
-                <div className='adress-fontsize'>R. Dr. Siqueira, 273 - Parque Dom Bosco, Campos dos Goytacazes - RJ, 28030-130</div>
+                <div className='adress-fontsize'>
+                  {campus}
+                </div>
               </div>
               <div className=" col-sm-6-off-set-3 mt-2 mb-2">
-              <Link  to="/event">
+              <Link  to={`/event/${id}`}>
                 <div className="custom-button btn w-100">Saiba mais</div>
               </Link>
               </div>
--- frontend/src/Screens/Events/Events.jsx
+++ frontend/src/Screens/Events/Events.jsx
@@ -1,29 +1,41 @@
 /*Criar Evento*/
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import EventCard from '../../Components/EventCard/EventCard';
 
-
 const Events = () => {
+    const [events, setEvents] = useState([]);
+
+    const getEvents = async ()  => {
+        try {
+            const response = await axios.get('http://localhost:8080/events');
+            const data = response.data;
+            setEvents(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getEvents();
+    },[]);
+
     return(
         <div className='container-fluid'>
             <div className='row'>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
-                <div className=' d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
-                    <EventCard/>
-                </div>
+                {events.map((evt) => (
+                    <div className=' mb-4 d-flex justify-content-center col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xl-6'>
+                        <EventCard 
+                        key={evt.id}
+                        id={evt.id}
+                        name={evt.name}
+                        campus={evt.campus}
+                        startDate={evt.startDate}
+                        endDate={evt.endDate}
+                        details={evt.details}
+                        folder={evt.folder} />
+                    </div>
+                ))}                
             </div>
         </div>
     )
